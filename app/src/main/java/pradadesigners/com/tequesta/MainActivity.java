8a907c8599e48b6ef63decc5eede92f7ea4a0aae
PradatiusD <danielprada2012@gmail.com>
2016-12-24 18:34:32 -0500

    Basic output of HTML onto homepage of app.

--- app/src/main/java/pradadesigners/com/tequesta/MainActivity.java
+++ app/src/main/java/pradadesigners/com/tequesta/MainActivity.java
@@ -2,12 +2,65 @@ package pradadesigners.com.tequesta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.statusText) TextView statusText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://www.westonfl.org/Departments/ParksAndRecreation/ParksStatus.aspx")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+
+                final String responseBody = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        statusText.setText(responseBody);
+                    }
+                });
+
+                Log.d("Tequesta", responseBody);
+            }
+        });
+
+
     }
 }
