96ede71cd93440d7df0c5e94dee7036122e62db9
PradatiusD <danielprada2012@gmail.com>
2016-12-25 11:31:06 -0500

    Refactored park data into separate class

--- app/src/main/java/pradadesigners/com/tequesta/MainActivity.java
+++ app/src/main/java/pradadesigners/com/tequesta/MainActivity.java
@@ -1,20 +1,12 @@
 package pradadesigners.com.tequesta;
 
-import android.nfc.Tag;
-import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -43,45 +35,6 @@ public class MainActivity extends AppCompatActivity {
         requestParkSiteHTML();
     }
 
-    private JSONArray convertParkSiteHTMLtoJSON (List<String> matchingGroups) throws JSONException {
-
-        JSONArray parksData = new JSONArray();
-        for (String matchingGroup : matchingGroups) {
-            JSONObject parkData = new JSONObject();
-
-            Pattern pattern = Pattern.compile("<td((\\S|\\s)*?)</td>");
-            Matcher matcher = pattern.matcher(matchingGroup);
-
-            while (matcher.find()) {
-                String match = matcher.group(1);
-                match = match.replaceAll("(\\n|\\r|\\t)","");
-                if (match.contains("statusImage")) {
-                    String parkStatus = match.contains("cell-14") ? "open" : "closed";
-                    parkData.put("status", parkStatus);
-                }
-                else if (match.contains("span")) {
-                    match = match.split("<br")[0];
-                    match = match.replaceAll("\\s{2,}","");
-                    parkData.put("name", match);
-                }
-                else if (match.contains(">Updated")) {
-                    parkData.put("lastUpdated", match.replace(" align=\"center\" width=\"150\">Updated ",""));
-                }
-                else if (match.contains("align")) {
-                    match = match.replace(" align=\"center\" width=\"125\">","");
-                    match = match.replaceAll("\\s{2,}","");
-                    match = match.replace("<br />",": ");
-                    parkData.put("comment", match);
-                }
-            }
-
-            parksData.put(parkData);
-        }
-
-        Log.d(TAG, parksData.toString());
-
-        return parksData;
-    }
 
     private void requestParkSiteHTML() {
         OkHttpClient client = new OkHttpClient();
@@ -103,28 +56,20 @@ public class MainActivity extends AppCompatActivity {
                     throw new IOException("Unexpected code " + response);
                 }
 
-
                 String responseBody = response.body().string();
 
                 Pattern pattern = Pattern.compile("rgRow.*(\\s|\\S)*?</tr");
                 Matcher matcher = pattern.matcher(responseBody);
 
-                List<String> matchingGroups = new ArrayList<>();
+                List<Park> parks = new ArrayList<>();
 
                 while (matcher.find()) {
                     String match = matcher.group(0);
-                    matchingGroups.add(match);
+                    Park park = new Park(match);
+                    parks.add(park);
                 }
 
-                JSONArray data = null;
-
-                try {
-                    data = convertParkSiteHTMLtoJSON(matchingGroups);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                final String status = data.toString();
+                final String status = parks.get(0).toString();
 
                 runOnUiThread(new Runnable() {
                     @Override
---
+++ app/src/main/java/pradadesigners/com/tequesta/Park.java
@@ -0,0 +1,85 @@
+package pradadesigners.com.tequesta;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by dprada on 12/25/16.
+ */
+
+public class Park {
+
+    private String mStatus;
+    private String mName;
+    private String mLastUpdated;
+    private String mComment;
+
+    public Park (String parkHTML) {
+
+        Pattern pattern = Pattern.compile("<td((\\S|\\s)*?)</td>");
+        Matcher matcher = pattern.matcher(parkHTML);
+
+        while (matcher.find()) {
+            String match = matcher.group(1);
+            match = match.replaceAll("(\\n|\\r|\\t)","");
+            if (match.contains("statusImage")) {
+                String parkStatus = match.contains("cell-14") ? "open" : "closed";
+                mStatus = parkStatus;
+            }
+            else if (match.contains("span")) {
+                match = match.split("<br")[0];
+                match = match.replaceAll("(\\s{2,}|>)","");
+                mName = match;
+            }
+            else if (match.contains(">Updated")) {
+                match = match.replace(" align=\"center\" width=\"150\">Updated ","");
+                mLastUpdated = match;
+            }
+            else if (match.contains("align")) {
+                match = match.replace(" align=\"center\" width=\"125\">","");
+                match = match.replaceAll("\\s{2,}","");
+                match = match.replace("<br />",": ");
+                mComment = match;
+            }
+        }
+    }
+
+    public String getStatus() {
+        return mStatus;
+    }
+
+    public void setStatus(String status) {
+        mStatus = status;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getLastUpdated() {
+        return mLastUpdated;
+    }
+
+    public void setLastUpdated(String lastUpdated) {
+        mLastUpdated = lastUpdated;
+    }
+
+    public String getComment() {
+        return mComment;
+    }
+
+    public void setComment(String comment) {
+        mComment = comment;
+    }
+
+    @Override
+    public String toString () {
+        return mName + "  " + mStatus + " " + mLastUpdated + " " + mComment;
+    }
+
+
+}
